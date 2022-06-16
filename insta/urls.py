b6335c57ddfd3e8bb3bebb8a081c72fc8ad4bc35
96olbap <alvinwanjala@gmail.com>
2022-06-16 16:01:59 +0300

    This commit will add the upload_post url and update the navbar link

--- insta/urls.py
+++ insta/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path(r'^search/', views.search_results, name='search_results'),
     path('register/', views.register, name = 'register'),
-
+    path('upload_post/', views.upload_post, name = 'upload_post')
 ]
