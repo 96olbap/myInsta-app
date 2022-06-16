33f648673692e197f9482888a2a6e5891dd88f63
96olbap <alvinwanjala@gmail.com>
2022-06-16 22:05:34 +0300

    This commit will improve the UI

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -2,6 +2,9 @@ const likeBtn = document.querySelector('.like-btn');
 let likeIcon = document.querySelector('#icon');
 let count = document.querySelector('#count');
 
+const bmBtn = document.querySelector('.bkmark-btn');
+let bmIcon = document.querySelector('#bookmark');
+
 
 // Btn clicked
 let clicked = false;
@@ -15,6 +18,18 @@ likeBtn.addEventListener('click', () => {
         clicked = false;
         likeIcon.innerHTML = `<i class="fa-regular fa-heart"></i>`
         count.textContent--;
+    }
+
+    console.log('hello');
+})
+
+bmBtn.addEventListener('click', () => {
+    if (!clicked) {
+        clicked = true;
+        bmIcon.innerHTML = `<i class="fa-solid fa-bookmark"></i>`
+    } else {
+        clicked = false;
+        bmIcon.innerHTML = `<i class="fa-regular fa-bookmark"></i>`
     }
 
     console.log('hello');
