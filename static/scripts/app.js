fcee1e8dbe24b06474ebfe4bd446ae09ab57ebbc
96olbap <alvinwanjala@gmail.com>
2022-06-16 21:53:42 +0300

    This commit will add like functionality

---
+++ static/scripts/app.js
@@ -0,0 +1,21 @@
+const likeBtn = document.querySelector('.like-btn');
+let likeIcon = document.querySelector('#icon');
+let count = document.querySelector('#count');
+
+
+// Btn clicked
+let clicked = false;
+
+likeBtn.addEventListener('click', () => {
+    if (!clicked) {
+        clicked = true;
+        likeIcon.innerHTML = `<i class="fa-solid fa-heart"></i>`
+        count.textContent++;
+    } else {
+        clicked = false;
+        likeIcon.innerHTML = `<i class="fa-regular fa-heart"></i>`
+        count.textContent--;
+    }
+
+    console.log('hello');
+})
